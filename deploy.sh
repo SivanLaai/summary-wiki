7fd1b1e34eb8f7f44ff3ddb05b7564672213377c
SivanLaai <lyhhap@163.com>
2022-03-02 19:59:41 +0800

    add doc or update doc

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ git push
 set -e
 
 # 生成静态文件
-#npm run build
+npm run build
 
 # 进入生成的文件夹
 mkdir -p docs/.vuepress/dist
@@ -53,4 +53,4 @@ git push -f $githubUrl master # 推送到github gh-pages分支
 # git push -f $codingUrl master # 推送到coding
 
 cd -
-#rm -rf docs/.vuepress/dist
+rm -rf docs/.vuepress/dist
